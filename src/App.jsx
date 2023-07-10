bbfe5382a4f513934517867accdc882bb2855568
Abhishek Bajpai <bajpaiabhishek1289@gmail.com>
2023-07-10 13:46:48 +0530

    aligned items to left

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function App() {
   let [runner, setRunner] = useState('')
   const [timeInterval, setTimeInterval] = useState(20);
-  const para = "The sentiment of these statements is mixed. Some express frustration and negative experiences with AT&T, particularly regarding issues with technology and privacy. Others express support for Randall Stephenson's decision to resign from the PGA Tour policy board due to concerns about the tour's deal with Saudi backers. There is also mention of controversy surrounding AT&T's involvement with Liv Golf and criticism of the internet's treatment of an AT&T woman."
+  const para = "Thhe sentiment of these statements is mixed. Some express frustration and negative experiences with AT&T, particularly regarding issues with technology and privacy. Others express support for Randall Stephenson's decision to resign from the PGA Tour policy board due to concerns about the tour's deal with Saudi backers. There is also mention of controversy surrounding AT&T's involvement with Liv Golf and criticism of the internet's treatment of an AT&T woman."
   const handleRunner = () => {
     let k = 0;
     const interval = setInterval(() => {
